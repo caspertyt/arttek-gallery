9d4c29bcffd3f9df6666a2adcbf54aabdca704d8
caspertyt <caspermladashiy6@gmail.com>
2025-05-05 00:32:19 +0200

    Fix: Change fallback language from ru to ua in dictionary

--- src/i18n/dictionary.ts
+++ src/i18n/dictionary.ts
@@ -1,5 +1,5 @@
 import { translations } from './config';
 
 export async function getDictionary(lang: string) {
-  return translations[lang as keyof typeof translations] || translations.ru;
+  return translations[lang as keyof typeof translations] || translations.ua;
 } 
